02cbcbefa485ce09503b1cda4acfe04dc0002189
cz <15553540096@163.com>
2018-04-17 14:48:14 +0800

    cz first commit

---
+++ src/main/java/czc/Hello.java
@@ -0,0 +1,17 @@
+package czc;
+
+
+
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+  
+@Controller  
+public class Hello {  
+    @RequestMapping(value="/Hello")  
+    public String HelloWorld(Model model){  
+        model.addAttribute("message","Hello World!!!");  
+        return "HelloWorld";  
+    }  
+      
+}  
